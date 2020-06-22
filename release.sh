4a64d5e969d322a0cf3b520272886eb6aa84b667
rohye <rohy.froze@gmail.com>
2020-06-22 08:53:23 +0800

    Update release script

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 npm run build
 docker build -t rohye-docs .
-docker tag rohye-docs:latest harbor.rohye.com:3000/rohye/rohye-docs:latest
-docker push harbor.rohye.com:3000/rohye/rohye-docs:latest
+docker tag rohye-docs:latest harbor.rohye.com/rohye/rohye-docs:latest
+docker push harbor.rohye.com/rohye/rohye-docs:latest
